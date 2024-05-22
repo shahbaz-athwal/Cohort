03b85147f609385a22f7a5a7d4f0f76924d8e8a1
Shahbaz <shahbazathwal2107@gmail.com>
2024-05-22 12:54:05 +0530

    done part 1

--- Practice/react-practise/src/App.jsx
+++ Practice/react-practise/src/App.jsx
@@ -1,30 +1,48 @@
 import React, { useState } from "react";
-import { useEffect } from "react"
+import { useEffect , memo} from "react"
 
 
 function App() {
-  
-  const [title, setTitle] = useState("My name is Shahbaz")
 
-  return (
-    <div>
-      <button onClick={changeTitle}>Change Title</button><br /><br />
-      <Header title={title} />
-      <Header title="sinfgfhggh" />
-      <Header title="singsdfsh" />
-      <Header title="singfsdsh" />
-    </div>
-  )
+  const [title, setTitle] = useState("My name is Shahbaz")
 
   function changeTitle(){
     setTitle("My name is " + Math.random());
   }
+  
+  return (
+    <div>
+      <HeaderWithButton />
+      <button onClick={changeTitle}>Change Title</button><br /><br />
+      <Header title={title} />
+      <Header title="singsdfsh" />
+      <Header title="singfsdsh" />
+      <Header title="singfsdsh" />
+      <Header title="singfsdsh" />
+      <Header title="singfsdsh" />
+    </div>
+  )
+  
 }
 
-function Header({title}) {
+function HeaderWithButton(){
+  const [title, setTitle] = useState("My name is Shahbaz")
+
+  function changeTitle(){
+    setTitle("My name is " + Math.random());
+  }
+
+  return <div>
+    <button onClick={changeTitle}>Change Title</button><br /><br />
+    <Header title={title} />
+  </div>
+}
+
+const Header = memo(function Header({title}) {
   return <div>
     {title}
   </div>
-}
+})
+
 
 export default App
