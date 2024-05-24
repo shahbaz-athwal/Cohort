cd9a534208d5a59114c1b6810a42bb7914846911
Shahbaz <shahbazathwal2107@gmail.com>
2024-05-24 12:50:50 +0530

    day 3

--- Practice/react-keys/src/App.jsx
+++ Practice/react-keys/src/App.jsx
@@ -3,22 +3,33 @@ import axios from "axios"
 import './App.css'
 
 function App() {
-    const [todos, setTodos] = useState([])
-
-    useEffect(() => {
-        axios.get("https://sum-server.100xdevs.com/todos")
-            .then((response) => {
-                setTodos(response.data.todos)
-            })
-    }, [])
+    
+    const [id, setId] = useState(1)
     return <>
-    {todos.map(todo => <Todo key={todo.id} title={todo.title} description={todo.description}/>)}
+    <button onClick={function(){
+        setId(4)
+    }}>1</button>
+    <button>2</button>
+    <button>3</button>
+    <button>4</button>
+    <Todo id={id}/>
     </>
+    
 }
 
-function Todo({title, description}) {
+
+function Todo({id}) {
+    const [todo, setTodo] = useState([])
+
+    useEffect(() => {
+        axios.get("https://sum-server.100xdevs.com/todo?id=" + id)
+            .then((response) => {
+                setTodo(response.data.todo)
+            })
+    }, [id])
+
     return <>
-    <div>{title} : {description}</div>
+    <h3>{todo.title} : {todo.description}</h3>
     </>
 }
 export default App
