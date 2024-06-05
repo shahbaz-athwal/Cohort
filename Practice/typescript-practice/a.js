d55070a8ede36ab99bdccee94036d0de2bd0f148
Shahbaz <shahbazathwal2107@gmail.com>
2024-06-05 15:03:26 +0530

    enum

--- Practice/typescript-practice/a.js
+++ Practice/typescript-practice/a.js
@@ -1,6 +1,12 @@
 "use strict";
-const teamLead = {
-    name: "harkirat",
-    startDate: new Date(),
-    department: "Software developer"
-};
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+function doSomething(keyPressed) {
+    // do something.
+}
+doSomething(Direction.Up);
--- Practice/typescript-practice/a.ts
+++ Practice/typescript-practice/a.ts
@@ -1,18 +1,12 @@
-type Employee = {
-    name: string;
-    startDate: Date;
-  };
-  
-  type Manager = {
-    name: string;
-    department: string;
-  };
-  
-  type TeamLead = Employee | Manager;
-  
-  const teamLead: TeamLead = {
-    name: "harkirat",
-    startDate: new Date(),
-    department: "Software developer"
-  };
-  
+enum Direction {
+  Up,
+  Down,
+  Left,
+  Right
+}
+
+function doSomething(keyPressed: Direction) {
+// do something.
+}
+
+doSomething(Direction.Up)
