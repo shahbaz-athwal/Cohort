3dfdceedf3ddca04e1df8bab51209b8e6ac7c486
Shahbaz <shahbazathwal2107@gmail.com>
2024-06-13 18:28:28 +0530

    Add Next.js files and configurations

---
+++ Practice/next-app/app/api/user/route.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+import { NextRequest, NextResponse } from "next/server";
+
+const client = new PrismaClient()
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
+}
